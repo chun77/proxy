4fe18e66783f5c2c29f4df1c03c8ca58c5f012bc
Evan Li <hl490@duke.edu>
2024-02-20 22:20:43 -0500

    cmake

---
+++ docker-deploy/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  erss-hwk2-hl490-zw297
+//
+//  Created by Evan on 2/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    // test
+}
