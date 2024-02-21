2ffd22d40ca182a0d616e7c7251e8ad91b8dfd21
Evan Li <hl490@duke.edu>
2024-02-20 22:20:03 -0500

    cmake

---
+++ docker-deploy/src/proxy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evan on 2/20/24.
+//
+
+#include "proxy.h"
---
+++ docker-deploy/src/proxy.h
@@ -0,0 +1,14 @@
+//
+// Created by Evan on 2/20/24.
+//
+
+#ifndef ERSS_HWK2_HL490_ZW297_PROXY_H
+#define ERSS_HWK2_HL490_ZW297_PROXY_H
+
+
+class proxy {
+
+};
+
+
+#endif //ERSS_HWK2_HL490_ZW297_PROXY_H
