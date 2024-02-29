a2f8e6604e206829fac49bc902844a4a740d9bb5
Evan Li <hl490@duke.edu>
2024-02-29 00:01:54 -0500

    a small bug

--- docker-deploy/src/client.cpp
+++ docker-deploy/src/client.cpp
@@ -4,82 +4,124 @@
 #include "proxy.h"
 #include "server.h"
 
+
+
 // handle the client connections
 void handle_client_connection(const std::string& ip, int id, int client_fd) {
-    std::string request = get_request(id, client_fd);
-    if (request.empty()) {
+    request_item request = get_request(id, client_fd);
+    if (request.content.empty()) {
         close(client_fd);
         return;
     }
 
-    // parse the request from the client
-    std::string first_line, method, path, httpVersion;
-    std::string hostname, port;
-
-    // parse the first line of the request
-    std::regex requestLineRegex(R"(^(\w+)\s+(\S+)\s+(HTTP/\d\.\d))");
-    std::smatch requestLineMatch;
-    if (std::regex_search(request, requestLineMatch, requestLineRegex) && requestLineMatch.size() > 3) {
-        first_line = requestLineMatch[0];
-        method = requestLineMatch[1];
-    }
-
-    // parse the host and port
-    std::regex hostRegex(R"(Host:\s*(\S+):(\d+))");
-    std::smatch hostMatch;
-    if (std::regex_search(request, hostMatch, hostRegex) && hostMatch.size() > 2) {
-        hostname = hostMatch[1];
-        port = hostMatch[2];
-    }
-
-    // current time
-    auto time_now= std::time(nullptr);
-    auto utc_time = *std::gmtime(&time_now);
+    // get the current time
+    auto time = std::time(nullptr);
+    auto utc_time = *std::gmtime(&time);
     std::string time_str = std::asctime(&utc_time);
-
-    log_writer.write(id, '"' + first_line + '"' + " from " + ip + "@" + time_str);
+    time_str.pop_back();
+    log_writer.write(id, '"' + request.first_line + '"' + " from " + ip + " @ " + time_str);
 
     // create a new connection to the server
-    int server_fd = connect_server(id, hostname, port);
+    int server_fd = connect_server(id, request.host, request.port);
     if (server_fd == -1) {
         log_writer.write(id, "ERROR: Fail to create a new connection to the server");
+        // send error message to the client
+        const char* response = "HTTP/1.1 502 Bad Gateway\r\n\r\n";
+        if (send(id, response, strlen(response), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send \"HTTP/1.1 502 Bad Gateway\" to the client");
+        } else{
+            log_writer.write(id, "Responding \"HTTP/1.1 502 Bad Gateway\" to the client");
+        }
         close(client_fd);
         return;
     }
 
-    if (method == "CONNECT") {
+    if (request.method == "CONNECT") {
         handle_connect(id, client_fd, server_fd);
-    } else if (method == "POST") {
-        handle_post(id, client_fd, server_fd, request);
-    } else if (method == "GET") {
-        handle_get(id, client_fd, server_fd, request);
+    } else if (request.method == "POST") {
+        handle_post(id, client_fd, server_fd, request.content);
+    } else if (request.method == "GET") {
+        cache_item in_cache = cache.check(request.first_line);
+        handle_get(id, client_fd, server_fd, request, in_cache);
+    } else {
+        log_writer.write(id, "WARNING: http method not supported");
+        const char* response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+        if (send(client_fd, response, strlen(response), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send \"HTTP/1.1 400 Bad Request\" to the client");
+        } else{
+            log_writer.write(id, "Responding \"HTTP/1.1 400 Bad Request\" to the client");
+        }
     }
+    close(server_fd);
     close(client_fd);
 }
 
-std::string get_request(int id, int client_fd) {
+// get the request from the client and parse it
+request_item get_request(int id, int client_fd) {
     const int buffer_size = 4096;
     char buffer[buffer_size];
     std::string buffer_str;
+    request_item item;
+
+    struct timeval tv{};
+    tv.tv_sec = 3;  // 3 seconds timeout
+    tv.tv_usec = 0;
+    setsockopt(client_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
 
     // receive the request from the client
     ssize_t bytes_received;
     while ((bytes_received = recv(client_fd, buffer, buffer_size, 0)) > 0) {
         buffer_str.append(buffer, bytes_received);
-    }
-    if (bytes_received < 0) {
-        // Send a 400 error code to the client
-        log_writer.write(id, "WARNING: Received nothing from the client");
-        const char* response = "HTTP/1.1 400 Bad Request\r\n\r\n";
-
-        if (send(client_fd, response, strlen(response), 0) < 0) {
-            log_writer.write(id, "WARNING: Fail to send HTTP/1.1 400 Bad Request to the client");
-        } else{
-            log_writer.write(id, "Responding HTTP/1.1 400 Bad Request to the client");
+        // if the request is complete
+        if (buffer_str.find("\r\n\r\n") != std::string::npos) {
+            break;
         }
-        return "";
     }
-    return buffer_str;
+    if (buffer_str.empty()) {
+        // Send a 400 error code to the client
+        log_writer.write(id, "ERROR: Received nothing from the client");
+        const char* response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+        if (send(client_fd, response, strlen(response), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send \"HTTP/1.1 400 Bad Request\" to the client");
+        } else{
+            log_writer.write(id, "Responding \"HTTP/1.1 400 Bad Request\" to the client");
+        }
+        return item;
+    }
+
+    item.content = buffer_str;
+    // parse the first line of the request
+    size_t first_space = buffer_str.find(' ');
+    if (first_space == std::string::npos) return item;
+    // get the method
+    item.method = buffer_str.substr(0, first_space);
+    size_t second_space = buffer_str.find(' ', first_space + 1);
+    if (second_space == std::string::npos) return item;
+
+    size_t line_end = buffer_str.find("\r\n", second_space + 1);
+    if (line_end == std::string::npos) {
+        line_end = buffer_str.length();
+    }
+    // get the first line of the request
+    item.first_line = buffer_str.substr(0, line_end);\
+
+    // parse the host and port
+    size_t host_pos = buffer_str.find("Host: ");
+    if (host_pos != std::string::npos) {
+        size_t start = buffer_str.find(' ', host_pos) + 1;
+        size_t end = buffer_str.find("\r\n", start);
+        std::string host_port = buffer_str.substr(start, end - start);
+        size_t colon_pos = host_port.find(':');
+        if (colon_pos != std::string::npos) {
+            item.host = host_port.substr(0, colon_pos);
+            std::istringstream(host_port.substr(colon_pos + 1)) >> item.port;
+        } else {
+            item.host = host_port;
+            item.port = "80"; // http default port
+        }
+    }
+
+    return item;
 }
 
 // handle the CONNECT method
@@ -88,10 +130,10 @@ void handle_connect(int id, int client_fd, int server_fd) {
     // Send a 200 OK response to the client
     const char* response_to_client = "HTTP/1.1 200 Connection established\r\n\r\n";
     if (send(client_fd, response_to_client, strlen(response_to_client), 0) < 0) {
-        log_writer.write(id, "WARNING: Fail to send HTTP/1.1 200 Connection established to the client");
+        log_writer.write(id, "ERROR: Fail to send \"HTTP/1.1 200 Connection established\" to the client");
         return;
     } else{
-        log_writer.write(id, "Responding HTTP/1.1 200 Connection established to the client");
+        log_writer.write(id, "Responding \"HTTP/1.1 200 Connection established\" to the client");
     }
 
     // forward the data between the client and the server
@@ -135,6 +177,57 @@ void handle_connect(int id, int client_fd, int server_fd) {
 void handle_post(int id, int client_fd, int server_fd, const std::string& request){
 
 }
-void handle_get(int id, int client_fd, int server_fd, const std::string& request){
+void handle_get(int id, int client_fd, int server_fd, request_item & request, cache_item &in_cache){
+    // if the request is not in the cache
+    if(in_cache.content.empty()){
+        log_writer.write(id, "not in cache");
 
+        // send the request to the server
+        if (send(server_fd, request.content.c_str(), request.content.size(), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send the request to the server");
+            return;
+        } else{
+            log_writer.write(id, "Requesting \"" + request.first_line + "\" from server " + request.host + ":" + request.port);
+        }
+
+        // get the response, parse it
+        cache_item response = get_response(id, server_fd, request);
+        if (response.content.empty() ) {
+            return;
+        }
+
+        // if the response is cacheable
+        if(check_cacheable(id, response)){
+            cache.add(request.first_line, response);
+        }
+
+        // send the response to the client
+        if (send(client_fd, response.content.c_str(), response.content.size(), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send the response to the client");
+        } else{
+            log_writer.write(id, "Responding \"" + response.first_line + "\" to client");
+        }
+    }
+    // need revalidation
+    else if (in_cache.need_validation || in_cache.expiration_time <= std::time(nullptr)){
+        if (in_cache.need_validation){
+            log_writer.write(id, "in cache, but requires re-validation");
+        } else{
+            std::string time_str = std::asctime(std::gmtime(&in_cache.expiration_time));
+            time_str.pop_back();
+            log_writer.write(id, "in cache, but expired at " +  time_str);
+        }
+       revalidate(id, client_fd, server_fd, request, in_cache);
+    }
+    else {
+        log_writer.write(id, "in cache, valid");
+        // send the response to the client
+        if (send(client_fd, in_cache.content.c_str(), in_cache.content.size(), 0) < 0) {
+            log_writer.write(id, "ERROR: Fail to send the response to the client");
+        } else{
+            log_writer.write(id, "Responding the response to the client");
+        }
+        return;
+    }
 }
+
--- docker-deploy/src/server.h
+++ docker-deploy/src/server.h
@@ -2,12 +2,14 @@
 // Created by Evan on 2/24/24.
 //
 #include "cache_log.h"
-
+#include "client.h"
 #ifndef MY_PROXY_RESPONSE_H
 #define MY_PROXY_RESPONSE_H
 
 int connect_server(int id, const std::string &hostname, const std::string &port);
-static bool is_cacheable(cache_item &response);
-static bool is_valid(cache_item &response);
-cache_item server_response_parse(const std::string &response, const std::string &url);
+bool check_cacheable(int id, cache_item &response);
+void revalidate(int id, int client_fd, int server_fd, request_item & request, cache_item &in_cache);
+
+// get and parse the response from the server
+cache_item get_response(int id, int server_fd, request_item &request);
 #endif //MY_PROXY_RESPONSE_H
